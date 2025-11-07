3390811af6aa9db0519ff441590070d381e71a03
SteveFau <stepan.fau@gmail.com>
2025-11-07 18:27:55 +0100

    state update queue challenge 1 start

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,12 @@
 'use client';
 
 import TrafficLight from "@/components/TrafficLight";
+import RequestTracker from "@/components/RequestTracker";
 
 export default function Home() {
   return (
     <>
-      <TrafficLight />
+      <RequestTracker />
     </>
   );
 }
