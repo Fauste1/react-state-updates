1ea63f5506d67610cbbb5969d9cb7418273feb16
SteveFau <stepan.fau@gmail.com>
2025-11-08 09:25:37 +0100

    red dot cursor start

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,12 @@
 
 import TrafficLight from "@/components/TrafficLight";
 import RequestTracker from "@/components/RequestTracker";
+import RedDotCursor from "@/components/RedDotCursor";
 
 export default function Home() {
   return (
     <>
-      <RequestTracker />
+      <RedDotCursor />
     </>
   );
 }
---
+++ src/components/RedDotCursor.js
@@ -0,0 +1,34 @@
+
+import { useState } from "react";
+
+export default function RedDotCursor() {
+
+    const [position, setPosition] = useState({ x: 0, y: 0});
+
+    return(
+        <div 
+            style={{
+                    position: 'relative',
+                    width: '100vw',
+                    height: '100vh',
+            }}
+            
+            onPointerMove={e => {
+                setPosition({ x: e.clientX, y: e.clientY })
+        }}>
+
+            <div 
+                style={{
+                    position: 'absolute',
+                    backgroundColor: 'red',
+                    borderRadius: '50%',
+                    transform: `translate(${position.x}px, ${position.y}px)`,
+                    left: -20,
+                    top: -20,
+                    width: 20,
+                    height: 20,
+            }} />
+        </div>
+    );
+
+}
